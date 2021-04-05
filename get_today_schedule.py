3e8d98be1ec307a3af1e471ae1b978164a5d346d
Jaehwan <dialektike@gmail.com>
2021-04-05 16:10:11 +0900

    Update get_today_schedule.py

--- get_today_schedule.py
+++ get_today_schedule.py
@@ -22,6 +22,26 @@ from bs4 import BeautifulSoup
 
 import pasing_page
 
+
+def chang_name_into_id_2021(team_name):
+    """2021년용 팀 이름을 팀 ID로 바꾸는 함수
+
+    2021년 SSG가 창단했다. 그래서 팀명이 SK에서 SSG로 변경되었다.
+    그러나 KBO 홈피에서 사용하는 ID는 안 바꾼 것 같다.
+    그래서 팀명을 KBO에서 바꾸는 함수를 새로 만들었다.
+
+    Args:
+        team_name (str): 팀명
+    
+    """
+    
+    list = {'KIA':'HT', '두산':'OB', '롯데':'LT', \
+            'NC':'NC', 'SSG':'SK', 'LG':'LG', \
+            '넥센': 'WO','키움':'WO', '히어로즈':'WO', '우리':'WO', \
+            '한화':'HH', '삼성': 'SS','KT':'KT'}
+
+    return list[team_name]
+
 if __name__ == "__main__":
 
     config = configparser.ConfigParser()
@@ -50,11 +70,11 @@ if __name__ == "__main__":
         i = i + 1
         temp_list = {
             "away":
-            pasing_page.chang_name_into_id_2021(
+            chang_name_into_id_2021(
                 item.find("div", class_="vs_lft").find_all("strong")[0].text
             ),
             "home":
-            pasing_page.chang_name_into_id_2021(
+            chang_name_into_id_2021(
                 item.find("div", class_="vs_rgt").find_all("strong")[0].text
             ),
             "state":
